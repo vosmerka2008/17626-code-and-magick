f3c9f2dae59a6571214524dc0da3f0df1a96fb28
Ulia Dumler <uliadumler@gmail.com>
2017-12-11 05:25:55 +0400

    Travis

--- js/dialog.js
+++ js/dialog.js
@@ -58,42 +58,41 @@
   var dialogHandle = setup.querySelector('.setup-user-pic');
 
   dialogHandle.addEventListener('mousedown', function (evt) {
-  evt.preventDefault();
+    evt.preventDefault();
 
-  var startCoords = {
-    x: evt.clientX,
-    y: evt.clientY
-  };
-
-  var onMouseMove = function (moveEvt) {
-    moveEvt.preventDefault();
-    console.log('clientX =' + moveEvt.clientX  + ' clientY =' + moveEvt.clientY);
-    var shift = {
-      x: startCoords.x - moveEvt.clientX,
-      y: startCoords.y - moveEvt.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
     };
 
-    startCoords = {
-      x: moveEvt.clientX,
-      y: moveEvt.clientY
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      if (moveEvt.clientY < 0 || moveEvt.clientY > window.innerHeight) {
+        setup.style.top = 0 + 'px';
+      } else {
+        setup.style.top = (setup.offsetTop - shift.y) + 'px';
+        setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+      }
     };
 
-    if (moveEvt.clientY < 0 || moveEvt.clientY > window.innerHeight) {
-      setup.style.top = 0 + 'px';
-    } else {
-      setup.style.top = (setup.offsetTop - shift.y) + 'px';
-      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
     };
-  };
 
-  var onMouseUp = function (upEvt) {
-    upEvt.preventDefault();
-
-    document.removeEventListener('mousemove', onMouseMove);
-    document.removeEventListener('mouseup', onMouseUp);
-  };
-
-  document.addEventListener('mousemove', onMouseMove);
-  document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
